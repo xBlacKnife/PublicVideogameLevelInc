604bfd3b99f6ca5769c82589fed5bdab87b6ea0c
Ernesto Aranda <ernestoadv@icloud.com>
2020-12-10 19:55:55 +0100

    Created basic components

--- web/phaser_game/src/app/app.module.ts
+++ web/phaser_game/src/app/app.module.ts
@@ -4,11 +4,25 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GameComponent } from './game/game.component';
+import { LoginComponent } from './login/login/login.component';
+import { HomeComponent } from './home/home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { JuegoComponent } from './juego/juego.component';
+import { GameplayerComponent } from './gameplayer/gameplayer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GameComponent
+    GameComponent,
+    LoginComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent,
+    JuegoComponent,
+    GameplayerComponent
   ],
   imports: [
     BrowserModule,
