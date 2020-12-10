f365a1cf92780f9cea23d47e333c1d267ebf3328
Ernesto Aranda <ernestoadv@icloud.com>
2020-12-10 20:28:49 +0100

    Created primeng config

--- web/phaser_game/src/app/primeng.ts
+++ web/phaser_game/src/app/primeng.ts
@@ -1,11 +1,15 @@
-// import { AccordionModule } from 'primeng/primeng';
-// import { PanelModule } from 'primeng/primeng';
-// import { ButtonModule } from 'primeng/primeng';
-// import { RadioButtonModule } from 'primeng/primeng';
+import { AccordionModule } from 'primeng';
+import { PanelModule } from 'primeng';
+import { ButtonModule } from 'primeng';
+import { RadioButtonModule } from 'primeng';
 
-// export class primengConfig { 
-//     AccordionModule
-//     PanelModule
-//     ButtonModule
-//     RadioButtonModule
-//  }
+@NgModule({
+    imports:  [
+        AccordionModule,
+        PanelModule,
+        ButtonModule,
+        RadioButtonModule
+    ]
+})
+
+export class PrimengConfig { }
