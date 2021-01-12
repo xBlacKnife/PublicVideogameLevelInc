adbcfdc8c8661b4b0c550e443da1413f7c3b6b44
Yifei Liu <37576347+luojuee@users.noreply.github.com>
2021-01-12 12:50:24 +0100

    interfaz del editor v0.1
    falta poner las botones

--- web/phaser_game/src/app/game/scenes/EditorScene.js
+++ web/phaser_game/src/app/game/scenes/EditorScene.js
@@ -25,19 +25,33 @@ class EditorScene extends Scene{
     //#region METODOS
 
     preload(){
+        this.load.image('editor_back_01', 'assets/game/images/editor/back_01.png');
+        this.load.image('editor_back_02', 'assets/game/images/editor/back_02.png');
+        this.load.image('editor_back_03', 'assets/game/images/editor/back_03.png');
+        this.load.image('editor_back_04', 'assets/game/images/editor/back_04.png');
+        this.load.image('editor_back_05', 'assets/game/images/editor/back_05.png');
 
+        this.load.image('editor_buttonBox', 'assets/game/images/editor/buttonBox.png');
     } // preload
 
 
     create(){
+        this.b1 = this.add.tileSprite(0, 0, this.game.config.width*2, this.game.config.height*2, 'editor_back_01');
+        this.b2 = this.add.tileSprite(0, 0, this.game.config.width*2, this.game.config.height*2, 'editor_back_02');
+        this.b3 = this.add.tileSprite(0, 0, this.game.config.width*2, this.game.config.height*2, 'editor_back_03');
+        this.b4 = this.add.tileSprite(0, 0, this.game.config.width*2, this.game.config.height*2, 'editor_back_04');
+        this.b5 = this.add.tileSprite(0, 0, this.game.config.width*2, this.game.config.height*2, 'editor_back_05');
 
-        this.add.text(0, 0, "EDIT");
-
+        this.add.image(0, 0, 'editor_buttonBox').setOrigin(0);
+        //this._buttons_manager = new ButtonManager(this, "menu_scene_buttons_config")
     } // create
 
 
     update(time, delta){
-
+        this.b5.tilePositionX += 0.4
+        this.b4.tilePositionX += 0.3
+		this.b3.tilePositionX += 0.2
+		this.b2.tilePositionX += 0.1
     } // update
 
     //#endregion
